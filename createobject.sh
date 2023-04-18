c2a4d5e824d47726b2f89e7aab0f15b0e5beeb3e
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-18 17:58:00 -0400

    add controller test

--- createobject.sh
+++ createobject.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -x
 SOURCE=$(mktemp -d)
 DEST=/Users/jeff/Downloads/demo
-tanzu acc generate "test-accelerator" --server-url https://accelerator.view.amer.end2end.link --options-file test.txt
-unzip test-accelerator.zip -d $SOURCE
-cd "${SOURCE}/test-accelerator";
-find . -type d  -print -exec mkdir -p "/Users/jeff/Downloads/demo/{}" \;
-find . -type f -exec cp {}  "/Users/jeff/Downloads/demo/{}" \;
-find . -type d -empty -delete
+#COMPONENT_NAME=$1
+#PACKAGE_NAME=$2
+
+ JAR=$(echo "{'foo'': '${SOURCE}'}" | jq .
+
+)
+
+  echo $JAR
+
+
+
+echo $MULTI_LINE_VAR
+#tanzu acc generate "test-accelerator" --server-url https://accelerator.view.amer.end2end.link --options-file << EOF {
+
+#unzip test-accelerator.zip -d $SOURCE
+#cd "${SOURCE}/test-accelerator";
+#find . -type d  -print -exec mkdir -p "/Users/jeff/Downloads/demo/{}" \;
+#find . -type f -exec cp {}  "/Users/jeff/Downloads/demo/{}" \;
+#find . -type d -empty -delete
