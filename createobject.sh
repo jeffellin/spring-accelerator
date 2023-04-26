05dc065ffd1cf2e096261d95aa60f4316dbc8033
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-25 21:12:54 -0400

    add script

--- createobject.sh
+++ createobject.sh
@@ -1,22 +1,29 @@
 #!/bin/bash -x
 SOURCE=$(mktemp -d)
 DEST=/Users/jeff/Downloads/demo
-#COMPONENT_NAME=$1
-#PACKAGE_NAME=$2
+COMPONENT_TYPE=$1
+PACKAGE_NAME=$2
+CLASS_NAME=$3
 
- JAR=$(echo "{'foo'': '${SOURCE}'}" | jq .
+JSON_STRING=$( jq -n -r \
+                  --arg cn "$CLASS_NAME" \
+                  --arg ct "$COMPONENT_TYPE" \
+                  --arg pn "$PACKAGE_NAME" \
+                  '{
+                    className : $cn, 
+                    componentType : $ct,
+                    includeTest : "yes",
+                    pkgName : $pn,
+                    projectName : "test-accelerator"}'
+                  )
 
-)
-
-  echo $JAR
+echo $JSON_STRING > test.txt
 
 
+tanzu acc generate test-accelerator --server-url https://accelerator.view.amer.end2end.link --options-file=test.txt
 
-echo $MULTI_LINE_VAR
-#tanzu acc generate "test-accelerator" --server-url https://accelerator.view.amer.end2end.link --options-file << EOF {
-
-#unzip test-accelerator.zip -d $SOURCE
-#cd "${SOURCE}/test-accelerator";
-#find . -type d  -print -exec mkdir -p "/Users/jeff/Downloads/demo/{}" \;
-#find . -type f -exec cp {}  "/Users/jeff/Downloads/demo/{}" \;
-#find . -type d -empty -delete
+unzip test-accelerator.zip -d $SOURCE
+cd "${SOURCE}/test-accelerator";
+find . -type d  -print -exec mkdir -p "/Users/jeff/Downloads/demo/{}" \;
+find . -type f -exec cp {}  "/Users/jeff/Downloads/demo/{}" \;
+find . -type d -empty -delete
