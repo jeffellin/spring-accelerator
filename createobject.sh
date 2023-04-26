15db3cfb6a7aecee190e03b4fe9605dad03b99d6
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-26 09:44:06 -0400

    update script

--- createobject.sh
+++ createobject.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash  -x
 SOURCE=$(mktemp -d)
 DEST=/Users/jeff/Downloads/demo
+PD=$(pwd)
 COMPONENT_TYPE=$1
 PACKAGE_NAME=$2
 CLASS_NAME=$3
@@ -22,8 +23,8 @@ echo $JSON_STRING > test.txt
 
 tanzu acc generate test-accelerator --server-url https://accelerator.view.amer.end2end.link --options-file=test.txt
 
-unzip test-accelerator.zip -d $SOURCE
+unzip -q test-accelerator.zip -d $SOURCE 
 cd "${SOURCE}/test-accelerator";
-find . -type d  -print -exec mkdir -p "/Users/jeff/Downloads/demo/{}" \;
-find . -type f -exec cp {}  "/Users/jeff/Downloads/demo/{}" \;
+find . -type d  -print -exec mkdir -p "$PD/{}" \;
+find . -type f -exec cp {}  "$PD/{}" \;
 find . -type d -empty -delete
